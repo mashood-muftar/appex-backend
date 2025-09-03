34a6acd425b7401f7761730ec4e30dc541b9e31f
Daniyal Khan <59561869+dkt147@users.noreply.github.com>
2025-09-03 17:29:06 +0500

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,15 +1,20 @@
-// src/config/firebaseConfig.js
 import admin from 'firebase-admin';
 import { serviceAccount } from './firebaseServiceAccount.js';
+import path from "path";
 
-// Initialize Firebase Admin
 try {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-  console.log('Firebase Admin initialized successfully');
+  if (!admin.apps.length) {
+    // admin.initializeApp({
+    //   credential: admin.credential.cert(serviceAccount)
+    // });
+    admin.initializeApp({
+        credential: admin.credential.cert(path.join('apex-biotics-firebase-adminsdk-fbsvc-19a4b06da4.json')),
+      });
+    console.log('Firebase Admin initialized successfully');
+  }
 } catch (error) {
   console.error('Error initializing Firebase Admin:', error);
 }
 
 export default admin;
+firebase.config
