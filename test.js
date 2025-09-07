e7f3258a89772ce7164d862198dbb15f3b3e32ad
Daniyal Khan <59561869+dkt147@users.noreply.github.com>
2025-09-08 03:49:17 +0500

    Update test.js

--- test.js
+++ test.js
@@ -6,9 +6,10 @@ const keyPath = "./apex-biotics-50aaad5e911e.json";
 console.log("Loading service account key from:", keyPath);
 console.log(fs.readFileSync(keyPath, "utf8").slice(0, 200), "..."); // show first 200 chars
 
-// Initialize Firebase
-const serviceAccount = require(keyPath);
+// Load service account JSON
+const serviceAccount = JSON.parse(fs.readFileSync(keyPath, "utf8"));
 
+// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
